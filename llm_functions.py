b5154f942d799d6ddb7dfaeccaa8b073d247aa34
Sanket-Kudtarkar-AI <skudtarkar16@gmail.com>
2023-07-06 16:25:50 +0530

    update for lamini model

--- llm_functions.py
+++ llm_functions.py
@@ -1,29 +1,38 @@
 import torch
-from transformers import LlamaTokenizer, LlamaForCausalLM, pipeline
+from transformers import pipeline
 from langchain.llms import HuggingFacePipeline
+from langchain import PromptTemplate, LLMChain
 
-#tokenizer = LlamaTokenizer.from_pretrained("eachadea/vicuna-7b-1.1")
-tokenizer = LlamaTokenizer.from_pretrained("models/vicuna-7b-1.1")
-model = LlamaForCausalLM.from_pretrained("models/vicuna-7b-1.1",
-                                         load_in_8bit=True,
-                                         device_map='auto',
-                                         torch_dtype=torch.float16,
-                                         low_cpu_mem_usage=True)
+# model_name = "MBZUAI/LaMini-Flan-T5-783M"
+model_name = "MBZUAI/LaMini-Flan-T5-248M"
 
-pipe = pipeline(
-    "text-generation",
-    model=model,
-    tokenizer=tokenizer,
-    max_length=2048,
-    temperature=0,
-    top_p=0.95,
-    repetition_penalty=1.15
-)
+pipe = pipeline("text2text-generation",
+                model=model_name,
+                max_length=2048,
+                device_map='cuda:0'
+                )
 
 llm = HuggingFacePipeline(pipeline=pipe)
 
 
-def generate_response(input_query):
-    llm_response = llm(input_query)
-    llm_response = str(llm_response)
+def generate_response(input_query, context=None):
+
+    input_documents = ""
+    if context:
+        prompt_template = """ 
+    
+        Answer the following question in more than 40 words.  
+    
+        Context : {context}
+    
+        Question : {question}
+    
+        Answer : """
+
+        prompt = PromptTemplate.from_template(prompt_template)
+
+        llm_chain = LLMChain(llm=llm, prompt=prompt)
+        llm_response = llm_chain.predict(context=context, question=input_query)
+    else:
+        llm_response = llm(input_query)
     return llm_response
